e778979b1dff93b113b3f2e9755bbb5a70a8125c
huanghankun <330140016@qq.com>
2019-03-15 17:32:21 +0800

    commit

--- src/test/java/com/example/sharding/ShardingApplicationTests.java
+++ src/test/java/com/example/sharding/ShardingApplicationTests.java
@@ -5,12 +5,25 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class ShardingApplicationTests {
 
     @Test
     public void contextLoads() {
+        String strs[]={"BB","AA","DD","CC","aA","Aa","aa","BBB"};
+        List<String> list = Arrays.asList(strs);
+        Arrays.sort(strs);
+        for( String str:strs){
+            System.out.println(str);
+        }
+        System.out.println("###############");
+        Collections.sort(list, (String a, String b) -> -1);
+        for( String str:list){
+            System.out.println(str);
+        }
     }
 
 }
