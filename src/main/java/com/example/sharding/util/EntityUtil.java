ff5dcbed75aa99026fea2f6a321a36fcbb14edea
huanghankun <330140016@qq.com>
2019-03-26 15:08:42 +0800

    对象管理类更新

--- src/main/java/com/example/sharding/util/EntityUtil.java
+++ src/main/java/com/example/sharding/util/EntityUtil.java
@@ -4,12 +4,16 @@ import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.util.*;
 
+
 /**
  * 对象属性的转化
  *
  * @author huanghankun
  */
 public class EntityUtil {
+
+    private static final char UNDER_LINE_CHAR = '_';
+
     private EntityUtil() {
     }
 
@@ -28,7 +32,7 @@ public class EntityUtil {
     public static <T> List<T> list2Entity(List<Map<String, Object>> list,
                                           Class<T> clazz) throws Exception {
 
-        List<T> entityList = new ArrayList<T>();
+        List<T> entityList = new ArrayList<>();
         for (Map<String, Object> map : list) {
             T entity = map2Entity(map, clazz);
             entityList.add(entity);
@@ -61,7 +65,8 @@ public class EntityUtil {
                 e.printStackTrace();
                 continue;
             }
-            field.setAccessible(true);// 暴力访问，取消age的私有权限。让对象可以访问
+            // 暴力访问，取消age的私有权限。让对象可以访问
+            field.setAccessible(true);
             try {
                 field.set(entity, value);
             } catch (IllegalArgumentException | IllegalAccessException e) {
@@ -82,7 +87,7 @@ public class EntityUtil {
     public static <T> Map<String, String> entityAttr2TableColumnMap(
             Class<T> clazz) {
         Field[] fields = clazz.getFields();
-        Map<String, String> map = new HashMap<String, String>();
+        Map<String, String> map = new HashMap<>(fields.length);
         for (int i = 0; i < fields.length; i++) {
             Field field = fields[i];
             map.put(field.getName(), entityAttrName2ColumnName(field.getName()));
@@ -123,6 +128,8 @@ public class EntityUtil {
 
     /**
      * 将对象属性转化成键值对的map
+     * <p>
+     * 日期:2019-03-26
      *
      * @param <T>
      * @param
@@ -162,7 +169,7 @@ public class EntityUtil {
                     mapNew.put(key, a);
                 } else if (value instanceof Collection || value instanceof Map) {
                     Map<String, Object> m = (Map<String, Object>) value;
-                    Map<String, Object> m1 = new HashMap<String, Object>();
+                    Map<String, Object> m1 = new HashMap<String, Object>(m.size());
                     for (String k : m.keySet()) {
                         Object o = m.get(k);
                         if (iSBaseType(o)) {
@@ -229,14 +236,33 @@ public class EntityUtil {
 
 
     /**
-     * 获得一个对象各个属性的字节流
+     * 获得一个对象各个属性
+     *
+     * @param entityName
+     * @return
+     * @throws Exception
      */
     public static Map<String, Object> getProperty(Object entityName) throws Exception {
-        Map<String, Object> map = new HashMap<String, Object>();
+        /**
+         * 1,获取对象的所属类
+         */
         Class<?> clszz = entityName.getClass();
+        /**
+         * 2,获取所有参数（包括父类）
+         */
         List<Field> list = getFieldList(clszz);
+        /**
+         * 3,将参数放入map
+         */
+        Map<String, Object> map = new HashMap<String, Object>(list.size());
         for (Field f : list) {
+            /**
+             * 3.1,获得对象属性的值
+             */
             Object value = invokeMethod(entityName, f.getName(), null);
+            /**
+             * 3.2,将参数放入map
+             */
             map.put(f.getName(), value);
         }
         return map;
@@ -249,8 +275,9 @@ public class EntityUtil {
      * @return
      */
     public static List<Field> getFieldList(Class<?> clszz) {
-        List<Field> fieldList = new ArrayList<Field>();
-        Field fields[] = clszz.getDeclaredFields();
+
+        Field[] fields = clszz.getDeclaredFields();
+        List<Field> fieldList = new ArrayList<Field>(fields.length);
         for (Field f : fields) {
             fieldList.add(f);
         }
@@ -265,25 +292,37 @@ public class EntityUtil {
     /**
      * 获得对象属性的值
      */
-    public static Object invokeMethod(Object owner, String methodName,
-                                      Object[] args) throws Exception {
+    public static Object invokeMethod(Object owner, String entityName, Object[] args) throws Exception {
+        /**
+         * 1,获取所属类
+         */
         Class<?> ownerClass = owner.getClass();
-        methodName = methodName.substring(0, 1).toUpperCase()
-                + methodName.substring(1);
-        Method method = null;
+        /**
+         * 2,根据熟悉拼装方法名【get + 属性名（属性名首字母大写】
+         */
+        String methodName = "get" + entityName.substring(0, 1).toUpperCase() + entityName.substring(1);
+
         try {
-            method = ownerClass.getMethod("get" + methodName);
+            /**
+             * 3，获取方法
+             */
+            Method method = ownerClass.getMethod(methodName);
+            /**
+             * 4,执行方法获取值并返回
+             */
+            return method.invoke(owner);
         } catch (SecurityException e) {
+            e.printStackTrace();
+            return null;
         } catch (NoSuchMethodException e) {
+            e.printStackTrace();
             return null;
         }
-        return method.invoke(owner);
+
     }
 
     /**
-     *
      * 遍历对象属性值(利用反射实现)，可以在需要对 对象中的每个字段都执行相同的处理时使用
-     *
      */
     public static List<String> checkFieldValueNull(Object bean) {
         List<String> varList = new ArrayList<String>();
@@ -299,9 +338,9 @@ public class EntityUtil {
                 if (!checkGetMet(methods, fieldGetName)) {
                     continue;
                 }
-                Method fieldGetMet = cls.getMethod(fieldGetName, new Class[] {});
-                Object fieldVal = fieldGetMet.invoke(bean, new Object[] {});
-                if (fieldVal != null && !"".equals(fieldVal)) {// StringUtils.isEmpty(fieldVal)
+                Method fieldGetMet = cls.getMethod(fieldGetName, new Class[]{});
+                Object fieldVal = fieldGetMet.invoke(bean, new Object[]{});
+                if (fieldVal != null && !"".equals(fieldVal)) {
                     varList.add(String.valueOf(fieldVal));
                 }
             } catch (Exception e) {
@@ -322,11 +361,11 @@ public class EntityUtil {
             return null;
         }
         int startIndex = 0;
-        if (fieldName.charAt(0) == '_') {
+        if (fieldName.charAt(0) == UNDER_LINE_CHAR) {
             startIndex = 1;
         }
         return "get" + fieldName.substring(startIndex, startIndex + 1).toUpperCase()
-                        + fieldName.substring(startIndex + 1);
+                + fieldName.substring(startIndex + 1);
     }
 
     /**
